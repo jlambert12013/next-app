2b507a3d26209e5393e972d660ecf9072ba95012
Jim Lambert <jimlambert@Jims-Air.attlocal.net>
2022-11-12 06:16:02 -0600

    First

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,12 @@
 export default function Head() {
   return (
-    <>
+    <head>
       <title>Alabama Powersports | 2925 Citizen Pkwy, Selma, AL</title>
       <meta
         name="description"
         content="Alabama Powersports provides the latest and best in powersports products and services in Selma, AL. We're happy to help you find either the perfect recreational vehicle or the parts you've been looking for. Call us or stop in today!L"
       />
       <link rel="icon" href="/favicon.ico" />
-    </>
+    </head>
   );
 }
